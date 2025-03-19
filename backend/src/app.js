c9f73f00c0166547b450cf91146f73b0cc1709f3
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-03-19 13:55:12 +0530

    add route path

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express")
-
+const aiRoute = require("./routes/aiRoute")
 const app = express();
 
-app.get("/",( req, res) => {
-    res.send("Hello World");
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/ai", aiRoute);
+
 
 module.exports = app
